f504c8109dca41d7f497f0c56c08e4dfdd54a826
Zach Cullen <zacullen@gmail.com>
2020-01-09 19:11:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
@@ -7,3 +9,18 @@ class Person
     end
   end
 end
+
+
+class Dog 
+  attr_accessor :name, :breed
+  
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+end
+
+binding.pry
+
+puts "goodbye"
